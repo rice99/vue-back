e404ea6e19c6deaa1ad1a7f4a57818fb3bc71ab4
kevin <946237542@qq.com>
2020-07-08 15:34:04 +0800

    update users Array

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 let http = require('http');
-let users = [{ id: 1, name: 'zhufeng1' }, { id: 2, name: 'zhufeng2' }, { id: 3, name: 'zhufeng3' }];
+let users = [
+    { id: 1, name: 'zhufeng1' },
+     { id: 2, name: 'kevin' }, 
+     { id: 3, name: 'lily' }];
 let server = http.createServer(function (req, res) {
     console.log(req.method, req.url);
     if (req.url == '/api/users') {
